272ebaef0a4ebdbcc0d28031ee77440c111a789f
Kasper Isager <kasperisager@gmail.com>
2014-10-01 19:57:49 +0200

    Import JUnit assertions by default.

--- generators/class/templates/ClassTest.java
+++ generators/class/templates/ClassTest.java
@@ -3,10 +3,11 @@
  */<% if (props.namespace || props.package) { %>
 package <% if (props.namespace) { %><%= props.namespace %><% } %><% if (props.package) { %><%= props.package %><% } %>;<% } %>
 
+import static org.junit.Assert.*;
 import org.junit.*;
 
 /**
- * <%= _.humanize(props.name) %> class unit test.
+ * <%= _.humanize(props.name) %> class unit tests.
  *
  * @version 1.0.0
  */
